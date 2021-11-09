c7b3ca563484492f117e51575e40ebdb9ec0f7a2
maaroufi sami <maaroufisami15@gmail.com>
2021-11-09 15:21:24 +0100

    creation module profil

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -14,7 +14,8 @@ const routes: Routes = [{path:'home', component: HomeComponent},
   {path:'', redirectTo: '/home' , pathMatch:'full'},
   { path: 'categorie', loadChildren: () => import('./categorie/categorie.module').then(m => m.CategorieModule) },
   { path: 'projet', loadChildren: () => import('./projet/projet.module').then(m => m.ProjetModule) },
-  { path: 'developpeur', loadChildren: () => import('./developpeur/developpeur.module').then(m => m.DeveloppeurModule) }
+  { path: 'developpeur', loadChildren: () => import('./developpeur/developpeur.module').then(m => m.DeveloppeurModule) },
+  { path: 'profil', loadChildren: () => import('./profil/profil.module').then(m => m.ProfilModule) }
 
 
  ]
