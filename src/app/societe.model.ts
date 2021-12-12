372890c9b0b58bdf7bcc678029cc6622b1b5ccc4
guembrinourhene <guembrinourhene@gmail.com>
2021-12-12 17:01:32 +0100

    societe details

---
+++ src/app/societe.model.ts
@@ -0,0 +1,10 @@
+export interface Societes{
+    id?: string;
+    tel?: string;
+    nom?: string;
+    date?: Date;
+    email?: string;
+    description?: string;
+    categorie?: string;
+    image?: string;
+}
